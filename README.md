e0e90a0548b72a5d33f1c0d5931b6c73b3a4bddc
leoalenccar <leoniuqalencar@gmail.com>
2026-03-03 10:43:38 -0300

    Versão 1.0

---
+++ README.md
@@ -0,0 +1,16 @@
+# Calculadora em C++
+
+Este projeto é um **exercício de aprendizado e evolução** de uma calculadora simples, desenvolvida em **C++**. O foco é treinar técnicas de desenvolvimento e melhorar o código com o tempo, incluindo novas funcionalidades e melhores práticas de programação.
+
+## Funcionalidades - Versão 1
+
+- **Operações básicas**:
+  - Soma
+  - Subtração
+  - Multiplicação
+  - Divisão
+
+- **Planejadas para versões futuras**:
+  - Adicionar novas operações como exponenciação e raízes quadradas.
+  - Implementação da calculadora com **Programação Orientada a Objetos (POO)**.
+  - Múltiplas operações.
---
+++ calculadora.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+void escolherOperacao()
+{
+    cout << "Qual o tipo de operação?" << endl;
+    cout << "Digite: " << endl;
+    cout << "'1' para soma;" << endl;
+    cout << "'2' para subtração;" << endl;
+    cout << "'3' para multiplicação;" << endl;
+    cout << "'4' para divisão." << endl;
+}
+
+double soma (double a, double b)
+{
+    return a + b;
+}
+
+double subt (double a, double b)
+{
+    return a - b;
+}
+
+double multiplicacao (double a, double b)
+{
+    return a * b;
+}
+
+double divisao (double a, double b)
+{
+    return a / b;
+}
+
+int main()
+{
+    double nmr1;
+    cout << "Digite um número: ";
+    cin >> nmr1;
+
+    int operacao;
+
+    escolherOperacao();
+
+    cin >> operacao;
+    if (operacao < 1 || operacao > 4)
+    {
+        cout << "Tipo de operação inválido";
+        return 0;
+    }
+
+    double nmr2;
+    cout << "Digite outro número: ";
+    cin >> nmr2;
+
+    switch (operacao)
+    {
+        case 1:
+            cout << nmr1 << " + " << nmr2 << " = " << soma(nmr1, nmr2) << endl;
+            break;
+        case 2:
+            cout << nmr1 << " - " << nmr2 << " = " << subt(nmr1, nmr2) << endl;
+            break;
+        case 3:
+            cout << nmr1 << " * " << nmr2 << " = " << multiplicacao(nmr1, nmr2) << endl;
+            break;
+        case 4:
+            if (nmr2 == 0)
+            {
+                cout << "Indefinido" << endl;
+            }
+            else
+            {
+                cout << nmr1 << " / " << nmr2 << " = " << divisao(nmr1, nmr2) << endl;
+            }
+            break;
+    }
+
+    return 0;
+}
